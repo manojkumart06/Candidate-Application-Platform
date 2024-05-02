814f4f41024e308755dc910ddf0e0859deefab52
MANOJ KUMAR <manojkumar06t@gmail.com>
2024-05-02 16:16:57 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import FilterBar from './components/FilterBar';
 
 function App() {
+
+  const [jobs, setJobs] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const body = JSON.stringify({
+    "limit": 10,
+    "offset": 50
+   });
+   
+   const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body
+   };
+
+   const fetchData = async () => {
+    try{
+      const response = await fetch('https://api.weekday.technology/adhoc/getSampleJdJSON?offset=' + offset + '&limit=10', requestOptions)
+      const data = await response.json();
+      const fulldata = data.jdList;
+      console.log('data is',data.jdList);
+      //setJobs(fulldata);
+      setJobs(prevJobs => [...prevJobs, ...fulldata]);
+      
+    }catch{
+      console.log('error occured');
+    }
+   };
+
+   useEffect(() => {
+      fetchData();
+   },[]);
+   
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search jobs</h1>
+      <div className='Filter-list'>
+      <FilterBar />
+      </div>
+      <div>
+        {jobs.map((item) => (
+          <>
+          <span>{item.jobRole}</span><br></br>
+          <span>{item.location}</span>
+          </>
+        )
+      )}
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/FilterBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import ListSubheader from '@mui/material/ListSubheader';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+const FilterBar = () => {
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel >Roles</InputLabel>
+        <Select native defaultValue="" id="grouped-native-select" label="Grouping">
+          <option aria-label="None" value="" />
+          <optgroup label="Category 1">
+            <option value={1}>Option 1</option>
+            <option value={2}>Option 2</option>
+          </optgroup>
+          <optgroup label="Category 2">
+            <option value={3}>Option 3</option>
+            <option value={4}>Option 4</option>
+          </optgroup>
+        </Select>
+      </FormControl>
+    </div>
+  )
+}
+
+export default FilterBar
