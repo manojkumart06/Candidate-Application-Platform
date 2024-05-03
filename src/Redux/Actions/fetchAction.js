bd772c753cb7fd258c40fa02a6354a5a981328d6
MANOJ KUMAR <manojkumar06t@gmail.com>
2024-05-03 10:36:41 +0530

    setting of Redux to manage the state of filters and job listings

---
+++ src/Redux/Actions/fetchAction.js
@@ -0,0 +1,26 @@
+//Action constants
+export const SET_FILTERS = 'SET_FILTERS';
+export const FETCH_JOBS_REQUEST = 'FETCH_JOBS_REQUEST';
+export const FETCH_JOBS_SUCCESS = 'FETCH_JOBS_SUCCESS';
+export const FETCH_JOBS_FAILURE = 'FETCH_JOBS_FAILURE';
+
+
+//Action creators
+export const setFilters = (filters) => ({
+  type: SET_FILTERS,
+  payload: filters,
+});
+
+export const fetchJobsRequest = () => ({
+  type: FETCH_JOBS_REQUEST,
+});
+
+export const fetchJobsSuccess = (jobs) => ({
+  type: FETCH_JOBS_SUCCESS,
+  payload: jobs,
+});
+
+export const fetchJobsFailure = (error) => ({
+  type: FETCH_JOBS_FAILURE,
+  payload: error,
+});
---
+++ src/Redux/Reducers/fetchReducer.js
@@ -0,0 +1,43 @@
+import { combineReducers } from 'redux';
+
+import {
+  SET_FILTERS,
+  FETCH_JOBS_REQUEST,
+  FETCH_JOBS_SUCCESS,
+  FETCH_JOBS_FAILURE,
+} from '../Actions/fetchAction'
+
+const initialState = {
+  filters: {},
+  jobs: [],
+  loading: false,
+  error: null,
+};
+
+const filtersReducer = (state = initialState.filters, action) => {
+  switch (action.type) {
+    case SET_FILTERS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const jobsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_JOBS_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_JOBS_SUCCESS:
+      return { ...state, loading: false, jobs: action.payload };
+    case FETCH_JOBS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+// combine the reducer functions here
+export const rootReducer = combineReducers({
+  filters: filtersReducer,
+  jobs: jobsReducer,
+});
